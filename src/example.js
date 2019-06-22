21352cda364122ac17ee0df5043dd45552e53ac5
James Bush <jbush@developertown.com>
2019-06-22 15:13:36 -0400

    Runs lint-fix to fix non-semantic syntactical errors

--- src/example.js
+++ src/example.js
@@ -3,9 +3,10 @@ var double = value => value * 2
 console.log(double(3))
 
 // ESLint
-var goodbye='Goodbye!'
+var goodbye = 'Goodbye!'
 
-function hello(){
-  return goodbye}
+function hello () {
+  return goodbye
+}
 
-if(false){}
+if (false) {}
